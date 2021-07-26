2c8499700484ae93d1ba5dac439475b105ef7f65
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2021-07-26 23:25:35 +0200

    Update README.md

--- ReassureTest.Net.Tests/DocumentationExamples.cs
+++ ReassureTest.Net.Tests/DocumentationExamples.cs
@@ -8,7 +8,7 @@ namespace ReassureTest.Tests
     {
         class Order
         {
-            public Guid Id{ get; set; }
+            public Guid Id { get; set; }
             public DateTime OrderDate { get; set; }
             public DateTime LatestDeliveryDate { get; set; }
             public Decimal TotalPrice { get; set; }
@@ -83,6 +83,62 @@ namespace ReassureTest.Tests
                     }
                 ]
             }");
+        }
+
+        [Test]
+        public void Order_and_oderlines_example_traditional()
+        {
+            var orderId = Guid.NewGuid();
+
+            var order = new Order()
+            {
+                Id = orderId,
+                OrderDate = DateTime.Now,
+                LatestDeliveryDate = new DateTime(2020, 02, 01),
+                TotalPrice = 26.98M,
+                OrderLines = new List<OrderLine>()
+                {
+                    new OrderLine()
+                    {
+                        Id = Guid.NewGuid(),
+                        Count = 3,
+                        OrderId = orderId,
+                        Name = "Rubber duck special",
+                        SKU = "RD17930827",
+                        Amount = 9.99M
+                    },
+                    new OrderLine()
+                    {
+                        Id = Guid.NewGuid(),
+                        Count = 1,
+                        OrderId = orderId,
+                        Name = "Sale 10%",
+                        SKU = null,
+                        Amount = 2.997M
+                    }
+                }
+            };
+
+            Assert.IsNotNull(order);
+            Assert.AreNotEqual(order.Id, Guid.Empty);
+            Assert.IsTrue((order.OrderDate-DateTime.Now).Duration() < TimeSpan.FromSeconds(2));
+            Assert.AreEqual(order.LatestDeliveryDate, new DateTime(2020, 02, 01));
+            Assert.AreEqual(order.TotalPrice, 26.98M);
+
+            Assert.AreEqual(order.OrderLines.Count, 2);
+            Assert.AreNotEqual(order.OrderLines[0].Id, order.Id);
+            Assert.AreEqual(order.OrderLines[0].Count, 3);
+            Assert.AreEqual(order.OrderLines[0].OrderId, order.Id);
+            Assert.AreEqual(order.OrderLines[0].Name, "Rubber duck special");
+            Assert.AreEqual(order.OrderLines[0].SKU, "RD17930827");
+            Assert.AreEqual(order.OrderLines[0].Amount, 9.99);
+
+            Assert.AreNotEqual(order.OrderLines[1].Id, order.Id);
+            Assert.AreEqual(order.OrderLines[1].Count, 1);
+            Assert.AreEqual(order.OrderLines[1].OrderId, order.Id);
+            Assert.AreEqual(order.OrderLines[1].Name, "Sale 10%");
+            Assert.AreEqual(order.OrderLines[1].SKU, null);
+            Assert.AreEqual(order.OrderLines[1].Amount, 2.997);
         }
     }
 }
